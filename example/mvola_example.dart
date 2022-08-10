d3059e0f0371c9a2a22005261bf64ab675c5c638
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2022-08-10 22:08:25 +0300

    Use .env file for example

--- example/mvola_example.dart
+++ example/mvola_example.dart
@@ -1,6 +1,9 @@
 import 'package:mvola/mvola.dart';
+import 'package:dotenv/dotenv.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  var env = DotEnv()..load();
+  var mvola = MVolaClient(env['CONSUMER_KEY']!, env['CONSUMER_SECRET']!);
+  var token = await mvola.generateAccessToken();
+  print(token);
 }
