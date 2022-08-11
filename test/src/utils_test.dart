bd30a2c3ff2367ae4ab899cd618e6f8ed04262af
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2022-08-11 18:58:00 +0300

    Improve test

--- test/src/utils_test.dart
+++ test/src/utils_test.dart
@@ -3,6 +3,9 @@ import 'package:test/test.dart';
 
 void main() {
   test('Date Formater', () {
-    expect(formatDate(DateTime.now()), '');
+    var dateFormat = RegExp('[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z');
+    var formatedDate = formatDate(DateTime.now());
+    expect(dateFormat.hasMatch(formatedDate), true);
+    expect(formatedDate.length, 24);
   });
 }
